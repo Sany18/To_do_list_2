3676ee3aa720fb7a061d1e1b998eb2fea9796373
Sany18 <parubok.sashko@gmail.com>
2018-10-31 10:04:06 +0200

    Build for deploy

---
+++ public/precache-manifest.c833111fdb22d4e8e29201a062f165d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0be6d68f697d5398f37e",
+    "url": "/static/js/main.0be6d68f.chunk.js"
+  },
+  {
+    "revision": "d80646987a731045fdfb",
+    "url": "/static/js/1.d8064698.chunk.js"
+  },
+  {
+    "revision": "0be6d68f697d5398f37e",
+    "url": "/static/css/main.885d7703.chunk.css"
+  },
+  {
+    "revision": "d80646987a731045fdfb",
+    "url": "/static/css/1.aac6c437.chunk.css"
+  },
+  {
+    "revision": "1de7f9457824731148d9e9d1091483c5",
+    "url": "/index.html"
+  }
+];
