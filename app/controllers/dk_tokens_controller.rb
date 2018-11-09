740ebd4410e8eb5a4524430a67d85106da077caa
Sany18 <parubok.sashko@gmail.com>
2018-11-09 17:26:15 +0200

    Fixed confirmation controller

--- app/controllers/dk_tokens_controller.rb
+++ app/controllers/dk_tokens_controller.rb
@@ -1,13 +1,21 @@
 class DkTokensController < Doorkeeper::TokensController
   # Overriding create action
   # POST /oauth/token
-  def create    
-    current_user ||= User.find(authorize_response.token.resource_owner_id) if authorize_response.token
-    if current_user.confirmed_email?
+  def create
+    if authorize_response.respond_to?(:token)
+      current_user = User.find(authorize_response.token.resource_owner_id)
+    else
+      current_user = nil
+    end
+
+    puts authorize_response.to_s
+    if current_user && current_user.confirmed_email?
       response = authorize_response
       headers.merge! response.headers
       self.response_body = response.body.to_json
       self.status = response.status
+    elsif !current_user
+      render json: { 'error' => 'Not validated data' }.to_json
     else
       render json: { 'error' => 'Check your mailbox. Email not confirmed' }.to_json
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "protected-taiga-81728.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "task-list-2.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       #smtp login:
